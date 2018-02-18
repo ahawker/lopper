689d76071bb876dd028a035415d34e0b3af5e666
Andrew Hawker <andrew.r.hawker@gmail.com>
2018-02-17 16:46:25 -0800

    Add 'app' module.
    This module contains the Chalice application and lambda handler
    for receiving and processing GitHub webhooks.

---
+++ lopper/app.py
@@ -0,0 +1,93 @@
+"""
+    lopper/app
+    ~~~~~~~~~~
+
+    Contains `chalice` app for running an AWS Lambda function responsible for receiving GitHub webhook requests.
+"""
+import chalice
+
+from lopper import auth, conf, hub, payload, response
+
+
+app = chalice.Chalice(app_name=conf.APPLICATION_NAME)
+
+
+@app.route('/lopper')
+def handler():
+    # Authorize the request by validating it's signature against our shared secret token.
+    resp = is_request_authentic(app.current_request)
+    if not resp:
+        return resp
+
+    # Example the request payload to determine if it's an event we should process.
+    resp = is_request_acceptable(app.current_request)
+    if not resp:
+        return resp
+
+    # Process the request with the goal of deleting the head branch of a merged pull request.
+    return process_request(app.current_request)
+
+
+def is_request_authentic(request, secret_token: str = conf.WEBHOOK_SECRET_TOKEN):
+    """
+    Examine the given request object to determine if it was sent by an authorized source.
+
+    :param request: Request object to examine for authenticity
+    :type request: :class:`~chalice.app.Request`
+    :param secret_token: Shared secret token used to create payload hash
+    :type: :class:`~str`
+    :return: Response object indicating whether or not the request is authentic
+    :rtype: :class:`~lopper.response.Response`
+    """
+    signature = request.headers.get('X-Hub-Signature')
+    if not signature:
+        return response.unauthorized('Missing "X-Hub-Signature" header')
+
+    return auth.is_authentic(signature, request.raw_body, secret_token)
+
+
+def is_request_acceptable(request,
+                          head_branch: str = conf.HEAD_BRANCH_PATTERN,
+                          base_branch: str = conf.BASE_BRANCH_PATTERN,
+                          repository_owner: str = conf.REPOSITORY_OWNER,
+                          repository_name: str = conf.REPOSITORY_NAME):
+    """
+    Examine the given request object to determine if it's a merged pull request that should be processed.
+
+    :param request: Request object to examine for authenticity
+    :type request: :class:`~chalice.app.Request`
+    :param head_branch: Regular expression to match head branches to accept
+    :type: :class:`~string`
+    :param base_branch: Regular expression to match base branches to accept
+    :type: :class:`~string`
+    :param repository_owner: Regular expression to match repository owners to accept
+    :type: :class:`~string`
+    :param repository_name: Regular expression to match repository names to accept
+    :return: Response object indicating whether or not the request should be further processed
+    :rtype: :class:`~lopper.response.Response`
+    """
+    # Nothing we can do if we received a request w/o a body or it wasn't using 'application/json' content-type.
+    body = request.json_body
+    if not body:
+        return response.unprocessable_entity('Request body is not JSON or empty')
+
+    return payload.is_acceptable_payload(body, head_branch, base_branch, repository_owner, repository_name)
+
+
+def process_request(request):
+    """
+    Examine the given request to find the merged head branch and invoke the GitHub API to delete it.
+
+    :param request: Request object to process deleting the merged head branch of
+    :type request: :class:`~chalice.app.Request`
+    :return: Response object indicating the success of deleting the merged head branch
+    :rtype: :class:`~lopper.response.Response`
+    """
+    # Nothing we can do if we received a request w/o a body or it wasn't using 'application/json' content-type.
+    body = request.json_body
+    if not body:
+        return response.unprocessable_entity('Request body is not JSON or empty')
+
+    # Grab ref of merged pull request head branch and delete it.
+    branch = payload.get_target_branch_ref(body)
+    return hub.delete_branch(branch)
